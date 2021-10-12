9d7bbc7b50466429a58832f4e1eeaea7413be90c
Anders Qvist <anders.qvist@xenit.se>
2021-10-12 10:21:06 +0200

    Publish a GitHub Action for gitops-promotion.
    Because GitHub only allows a single action in each repo, we add
    a custom endpoint that allows the action to pass the command as
    an environment var along the rest of the arguments.
    
    The current version of gitops-promotion has some quirk in how it
    behaves that means it needs access to the Git history. This is
    problematic in that the default GitHub checkout Action defaults
    to --depth=1, prompting gitops-promotion to fail with "object not
    found - no match for id (SHA)" when pushing the promotion branch.
    Despite enthusiastic testing with plumbing commands, I cannot
    reproduce this state locally, and so this limitation will have to
    stand for now.
    
    Signed-off-by: Anders Qvist <anders.qvist@xenit.se>

---
+++ action-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+case $ACTION in
+    new)
+        /usr/local/bin/gitops-promotion new \
+            -provider github \
+            -sourcedir "$GITHUB_WORKSPACE" \
+            -token "$TOKEN" \
+            -group "$GROUP" \
+            -app "$APP" \
+            -tag "$TAG"
+        ;;
+    promote)
+        /usr/local/bin/gitops-promotion promote \
+            -provider github \
+            -sourcedir "$GITHUB_WORKSPACE" \
+            -token "$TOKEN"
+        ;;
+    *)
+        echo "Unkown action $ACTION"
+        exit 1
+esac
