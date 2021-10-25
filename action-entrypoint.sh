1d651a66de7ccab74d0935602a5cf8fcc5450819
Anders Qvist <anders.qvist@xenit.se>
2021-10-25 09:57:07 +0200

    GitHub Action support for status action.

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -18,6 +18,12 @@ case $ACTION in
             -sourcedir "$GITHUB_WORKSPACE" \
             -token "$TOKEN"
         ;;
+    status)
+        /usr/local/bin/gitops-promotion status \
+            -provider github \
+            -sourcedir "$GITHUB_WORKSPACE" \
+            -token "$TOKEN"
+        ;;
     *)
         echo "Unkown action $ACTION"
         exit 1
